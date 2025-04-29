b58cbdabdd8dc90ed858c910609f235298ddebf1
AIhaisi <1787669829@qq.com>
2025-04-29 14:25:47 +0800

    change main.vue

--- src/pages/main.vue
+++ src/pages/main.vue
@@ -6,8 +6,10 @@
 
     <body>
         <div id="main">
-        <h1>Welcome to Vue Gallery!</h1>
-        <h1>Hello</h1>
+            <div id="text">
+                <h1>Welcome to Vue Gallery!</h1>
+                <h1>Hello</h1>
+            </div>
         </div>
 
 
@@ -23,14 +25,22 @@
     font-family: Arial, Helvetica, sans-serif;
 
 }
-body div{
-    position: absolute ;
-    top:0 ;
-    left:20px ;
+
+body div {
+    position: absolute;
+    top: 0;
+    left: 0;
     width: 100%;
     height: 100vh;
-    background: linear-gradient(0.25turn, rgb(21, 19, 31), #9e4be2, #350fa9,rgb(21, 19, 31));
+    background: linear-gradient(0.5turn, rgb(21, 19, 31), #9e4be2, #350fa9, rgb(21, 19, 31));
+    overflow: hidden;
 }
+
+#text {
+    left: 20px;
+    background-color: transparent;
+}
+
 h1 {
     font-size: 40px;
     text-align: left;
